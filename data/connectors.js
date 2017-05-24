4d76ed845f6cfbbdd476502bbd4517732103e115
Alan Henson <alan.henson@optifysoftware.com>
2017-05-24 12:33:36 -0500

    Added support for Mongo and added IntelliJ project files.

--- data/connectors.js
+++ data/connectors.js
@@ -1,3 +1,4 @@
+import Mongoose from 'mongoose';
 import Sequelize from 'sequelize';
 import casual from 'casual';
 import _ from 'lodash';
@@ -7,36 +8,50 @@ const db = new Sequelize('blog', null, null, {
   storage: './blog.sqlite',
 });
 
+const mongo = Mongoose.connect('mongodb://localhost/views');
+
 const AuthorModel = db.define('author', {
-  firstName: { type: Sequelize.STRING },
-  lastName: { type: Sequelize.STRING },
+  firstName: {type: Sequelize.STRING},
+  lastName: {type: Sequelize.STRING},
 });
 
 const PostModel = db.define('post', {
-  title: { type: Sequelize.STRING },
-  text: { type: Sequelize.STRING },
+  title: {type: Sequelize.STRING},
+  text: {type: Sequelize.STRING},
 });
 
 AuthorModel.hasMany(PostModel);
 PostModel.belongsTo(AuthorModel);
 
+const ViewSchema = Mongoose.Schema({
+                                     postId: Number,
+                                     views: Number,
+                                   });
+
 // create mock data with a seed, so we always get the same
 casual.seed(123);
-db.sync({ force: true }).then(() => {
+db.sync({force: true}).then(() => {
   _.times(10, () => {
     return AuthorModel.create({
-      firstName: casual.first_name,
-      lastName: casual.last_name,
-    }).then((author) => {
+                                firstName: casual.first_name,
+                                lastName: casual.last_name,
+                              }).then((author) => {
       return author.createPost({
-        title: `A post by ${author.firstName}`,
-        text: casual.sentences(3),
+                                 title: `A post by ${author.firstName}`,
+                                 text: casual.sentences(3),
+                               }).then((post) => { // <- the new part starts here
+        // create some View mocks
+        return View.update(
+          {postId: post.id},
+          {views: casual.integer(0, 100)},
+          {upsert: true});
       });
     });
   });
 });
 
+const View = Mongoose.model('views', ViewSchema);
 const Author = db.models.author;
 const Post = db.models.post;
 
-export { Author, Post };
+export {Author, Post, View};

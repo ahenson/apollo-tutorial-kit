065d0afa740b3d7ffb9963cb350a19191799486b
Alan Henson <alan.henson@optifysoftware.com>
2017-05-24 12:11:32 -0500

    Efforts up to step 6 in the tutorial: https://dev-blog.apollodata.com/tutorial-building-a-graphql-server-cddaa023c035

--- data/schema.js
+++ data/schema.js
@@ -1,11 +1,28 @@
 const typeDefinitions = `
+type Author {
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String 
+  text: String
+  views: Int
+  author: Author
+}
+
 type Query {
-  testString: String
+  author(firstName: String, lastName: String): Author
+  getFortuneCookie: String # we'll use this later
 }
 
 schema {
   query: Query
 }
+
 `;
 
 export default [typeDefinitions];
